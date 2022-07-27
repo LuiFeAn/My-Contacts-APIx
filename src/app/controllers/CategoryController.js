e8925693cdc2e3cda91786ed7494532264490d2d
Luis Fernando <luisfernandogvv@gmail.com>
2022-07-27 18:29:15 -0300

    Criando Controller de categorias

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,26 @@
+class CategoryController {
+
+    async index(req,res){
+
+    }
+
+    async show(req,res){
+
+    }
+
+    async store(req,res){
+
+    }
+
+    async delete(req,res){
+
+    }
+
+    async update(req,res){
+
+    }
+
+
+}
+
+module.exports = new CategoryController();
