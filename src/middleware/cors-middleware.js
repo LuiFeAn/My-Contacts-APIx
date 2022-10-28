a38c863172e4f4b67cb31820cf26ef81ac5d0128
luis <luisfernandogvv@gmail.com>
2022-10-28 17:38:51 -0300

    Definindo header Acess-Control-Allow-Origin-Header com WildCard

--- src/middleware/cors-middleware.js
+++ src/middleware/cors-middleware.js
@@ -1,5 +1,6 @@
 module.exports = (req,res,next) => {
     res.setHeader('Acess-Control-Allow-Origin',process.env.CORS_ORIGIN);
     res.setHeader('Acess-Control-Allow-Origin-Methods','*');
+    res.setHeader('Acess-Control-Allow-Origin-Headers','*');
     next();
 }
