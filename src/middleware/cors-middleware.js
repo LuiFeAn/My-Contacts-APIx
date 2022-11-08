0654f590dc1e54b668351eb01dab4cbd68bab549
luis <luisfernandogvv@gmail.com>
2022-11-08 19:45:39 -0300

    Modificando Header CORS para aceitar todos os verbos HTTP

--- src/middleware/cors-middleware.js
+++ src/middleware/cors-middleware.js
@@ -1,7 +1,7 @@
 module.exports = (req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin',process.env.CORS_ORIGIN);
-    res.setHeader('Access-Control-Allow-Origin-Methods','*');
-    res.setHeader('Access-Control-Allow-Origin-Headers','*');
+    res.setHeader('Access-Control-Allow-Methods','*');
+    res.setHeader('Access-Control-Allow-Headers','*');
     res.setHeader('Access-Control-Max-Age','86400');
     next();
 }
